4034ff7fcea743a0a46942ec83cfda8f1563a88c
Elvis Brevi <elvisbrevi@gmail.com>
2025-10-27 09:51:57 -0300

    [Keyboard] Remove unused custom keycodes and adjust encoder mapping for Sofle Choc keymap

--- keyboards/sofle_choc/keymaps/split-wire-cone/keymap.c
+++ keyboards/sofle_choc/keymaps/split-wire-cone/keymap.c
@@ -22,7 +22,7 @@ enum sofle_layers {
     _ADJUST,
 };
 
-enum custom_keycodes { KC_QWERTY = SAFE_RANGE, KC_LOWER, KC_RAISE, KC_ADJUST, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE };
+enum custom_keycodes { KC_QWERTY = SAFE_RANGE, KC_PRVWD, KC_NXTWD, KC_LSTRT, KC_LEND, KC_DLINE };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *            `----------------------------------'           '------''---------------------------'
      */
 
-    [_QWERTY] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, KC_TAB, KC_LBRC, KC_RBRC, KC_SPC, KC_SPC, KC_LALT, MO(_RAISE), KC_LCBR, KC_RCBR, KC_PLUS),
+    [_QWERTY] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, XXXXXXX, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, KC_TAB, KC_LBRC, KC_RBRC, KC_BSLS, KC_LALT, KC_SPC, MO(_RAISE), KC_LCBR, KC_RCBR, KC_PLUS),
 
     /*
      * LOWER
@@ -77,21 +77,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_RAISE] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_INS, KC_PSCR, KC_UP, KC_APP, XXXXXXX, KC_PRVWD, KC_NXTWD, KC_DLINE, XXXXXXX, XXXXXXX, KC_BSPC, _______, KC_LALT, KC_LEFT, KC_DOWN, KC_RGHT, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL, XXXXXXX, KC_BSPC, _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX, _______, _______, KC_LSTRT, XXXXXXX, KC_LEND, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
-    /* ADJUST
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | RESET|      |QWERTY|      |      |      |                    |      |      |      |      |      |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |      |      |MACWIN|      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
-     * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
-     * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
-     * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *            |      |      |      |      | /       /       \      \ |      |      |      |      |
-     *            |      |      |      |      |/       /         \      \|      |      |      |      |
-     *            `----------------------------------'           '------''---------------------------'
-     */
-    [_ADJUST] = LAYOUT(XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX, KC_QWERTY, XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)};
+#ifdef ENCODER_MAP_ENABLE
+
+    const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+        [_QWERTY] = {{KC_VOLU, KC_VOLD}, {KC_MNXT, KC_MPRV}},
+        [_LOWER]  = {{KC_VOLU, KC_VOLD}, {KC_MNXT, KC_MPRV}},
+        [_RAISE]  = {{KC_VOLU, KC_VOLD}, {KC_MNXT, KC_MPRV}},
+        [_ADJUST] = {{KC_VOLU, KC_VOLD}, {KC_MNXT, KC_MPRV}},
+    };
+
+#endif
 
 #ifdef OLED_ENABLE
 
@@ -161,31 +156,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_QWERTY:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_QWERTY);
-            }
-            return false;
-        case KC_LOWER:
-            if (record->event.pressed) {
-                layer_on(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_LOWER);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_RAISE:
-            if (record->event.pressed) {
-                layer_on(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            } else {
-                layer_off(_RAISE);
-                update_tri_layer(_LOWER, _RAISE, _ADJUST);
-            }
-            return false;
-        case KC_ADJUST:
-            if (record->event.pressed) {
-                layer_on(_ADJUST);
-            } else {
-                layer_off(_ADJUST);
             }
             return false;
         case KC_PRVWD:
